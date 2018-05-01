04acf2e7ab6ab589c9b991e20c391a3abf22860d
jtatanasov <jtatanasov@gmail.com>
2018-05-01 12:51:42 +0300

    user can add to cart + modal dialog after that

--- public/assets/js/books/bookDetailsController.js
+++ public/assets/js/books/bookDetailsController.js
@@ -9,15 +9,17 @@
         vm.rating = {};
         vm.isAvailable = true;
         vm.isLogged = false;
+        vm.addedToCart = false;
 
-        
+
         BooksService.getBookById(bookId).then(function (response) {
             if (!$rootScope.user) {
                 vm.isLogged = false;
             } else {
+                vm.userId = $rootScope.user._id;
                 vm.isLogged = true;
             }
-    
+
             vm.bookDetails = response.data;
             if (vm.bookDetails.volumeInfo.quantity == 0) {
                 vm.isAvailable = false;
@@ -52,8 +54,40 @@
         }
 
 
-        vm.addToCart = function() {
-            CartService.addToCart();
+
+        //adding to cart + modal dialog
+        vm.addToCart = function () {
+            CartService.addToCart(vm.userId, vm.bookDetails)
+                .then(resp => {
+                    if(resp.data.alreadyInCart) {
+                        vm.addedToCart = true;
+                    } else {
+                        vm.addedToCart = false;
+                    }
+                    angular.element('#show-modal-dialog').addClass('show');
+                    angular.element('#show-modal-dialog').css('display', 'block');
+                    angular.element('#show-modal-dialog').css('padding-right', '17px');
+                    angular.element('body').addClass('modal-open');
+                    angular.element('body').append("<div id='help-div' class='modal-backdrop fade show'></div>");
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+
+        function closeModalDialog() {
+            angular.element('#show-modal-dialog').removeClass('show');
+            angular.element('#show-modal-dialog').css('display', 'none');
+            angular.element('#show-modal-dialog').css('padding-right', '17px');
+            angular.element('body').removeClass('modal-open');
+            angular.element('#help-div').remove();
+        }
+        vm.closeModal = function () {
+            closeModalDialog();
+        }
+        vm.goToCart = function() {
+            closeModalDialog();
+            $location.path('/profile/cart');
         }
     }
-})();
+})(); 
--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+function Cart(userId) {
+    this.userId = userId;
+    this.books = [];
+    this.price = 0;
+    this.deliveryPrice = 0;
+    this.totalPrice = 0;
+}
 function calculatePrice(cart) {
     var price = 0;
     price = cart.books.reduce(function (price, book) {
@@ -54,6 +61,41 @@ router.get('/:id', function (req, res, next) {
     });
 });
 
+router.post('/:userId', function(req,res,next) {
+    var cartsCollection = req.db.get('carts');
+    var userId = req.params.userId;
+    var bookToAdd = req.body;
+    cartsCollection.find({userId: userId}, {}, (err, docs) => {
+        if(err) throw err;
+        else {
+            if(docs.length == 0) {
+                var cart = new Cart(userId);
+                cartsCollection.insert(cart);
+            } else {
+                var cart = docs[0];
+            }
+            var booksInCart = cart.books;
+
+            if(!booksInCart.find(b => b.bookId == bookToAdd.bookId)) {
+                booksInCart.push(bookToAdd);
+            } else {
+                res.status(200);
+                res.json({alreadyInCart: true});
+                return;
+            }
+
+            cart = updateCart(cart, booksInCart);
+
+            cartsCollection.update({ _id: cart._id }, cart, function (err, docs) {
+                if (err) throw err
+                else {
+                    res.status(200);
+                    res.json({ message: 'success' });
+                }
+            })
+        }
+    });
+});
 router.delete('/:userId', function (req, res, next) {
     var cartsCollection = req.db.get('carts');
     var userId = req.params.userId;
