1033ebf88abfd39b70c42532882d63c94b2d8b3f
Kate <katerina.d.ignatova@gmail.com>
2018-05-10 12:21:19 +0300

    minor fixes

--- public/assets/js/categories/categoriesService.js
+++ public/assets/js/categories/categoriesService.js
@@ -9,7 +9,7 @@
         this.getCategories = function() {
             return new Promise(function(resolve, reject) {
                 if (!categories) {
-                    $http.get('assets/js/directives/categories/categories.json').then(function(response) {
+                    $http.get('assets/data/categories.json').then(function(response) {
                             categories = response.data;
                             resolve(categories);
                         }).catch(function(err) {
